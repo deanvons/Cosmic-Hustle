bbb23b4a1f0897f5cbd04fd1428634a991c22a0f
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-10-16 13:23:40 +0200

    FEAT: added Merchant and Ledger classes

--- Cosmic Hustle Test Harness/ModelTests.cs
+++ Cosmic Hustle Test Harness/ModelTests.cs
@@ -1,10 +1,59 @@
+using Cosmic_Hustle_Core.Models;
+
 namespace Cosmic_Hustle_Test_Harness
 {
-    public class UnitTest1
+    public class ModelTests
     {
         [Fact]
-        public void Test1()
+        public void Merchant_shouldbe_created_with_provided_prop_values()
         {
+
+            // Arrange
+            var expectedId = 1;
+            var expectedName = "name";
+
+            // Act
+            var merchant = new Merchant(expectedId, expectedName);
+
+            // Assert
+            Assert.Equal(expectedId, merchant.Id);
+            Assert.Equal(expectedName, merchant.Name);
+
+        }
+
+        [Fact]
+        public void Merchant_shouldbe_created_with_ledger()
+        {
+
+            // Arrange
+            var expectedId = 1;
+            var expectedName = "name";
+
+            // Act
+            var merchant = new Merchant(expectedId, expectedName);
+
+            // Assert
+
+            Assert.NotNull(merchant.Ledger);
+
+        }
+
+        [Fact]
+        public void Ledger_shouldbe_created_with_provided_prop_values()
+        {
+
+            // Arrange
+            var expectedId = 1;
+
+
+            // Act
+            var ledger = new Ledger(expectedId);
+
+
+            // Assert
+            Assert.Equal(expectedId, ledger.Id);
+
+
 
         }
     }
