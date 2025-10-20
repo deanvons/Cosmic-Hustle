120077f4531fa2639c8b39b9da7b33c7e7bf751c
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-10-20 13:56:19 +0200

    FEAT: added core models

---
+++ Cosmic Hustle Core/Models/MarketListing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmic_Hustle_Core.Models
+{
+    public class MarketListing
+    {
+        public MarketListing(int id, double price, WareItemCache wareItemCache)
+        {
+            Id = id;
+            Price = price;
+            WareItemCache = wareItemCache;
+        }
+
+        public int Id { get; set; }
+
+        public WareItemCache WareItemCache { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
---
+++ Cosmic Hustle Core/Models/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmic_Hustle_Core.Models
+{
+    public class Ship
+    {
+        public Ship(int id, string name, double cargoCapacity, double fuelCapacity, double cargoLoad, double fuelLevel, int solarSystemTravelRange)
+        {
+            Id = id;
+            Name = name;
+            CargoCapacity = cargoCapacity;
+            FuelCapacity = fuelCapacity;
+            CargoLoad = cargoLoad;
+            FuelLevel = fuelLevel;
+            SolarSystemTravelRange = solarSystemTravelRange;
+        }
+
+        public int Id { get;set; }
+        public string Name { get; set; }
+        public double CargoCapacity { get; set; }
+        public double FuelCapacity { get; set; }
+        public double CargoLoad { get; set; }
+        public double FuelLevel { get; set; }
+        public int SolarSystemTravelRange { get; set; }
+
+        public List<WareItem> CargoBay { get; set; } = new List<WareItem>();
+
+    }
+}
